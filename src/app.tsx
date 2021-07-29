6c8576c740d2ec984854aa4b66f7c215d584c52d
shinudesu <arokidaaron93@gmail.com>
2021-07-30 07:43:41 +0800

    Added eslint

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import './app.scss';
 import ApplicationRoutes from './routes';
+import './app.scss';
 
 const Application: React.FC = () => {
   return (
